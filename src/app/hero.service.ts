54c30c77f41c12d1e973ff024d6fbe813b8abe36
Alex M <milesAwayAlex@gmail.com>
2021-12-15 11:17:23 -0800

    Implement the add hero function

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -58,11 +58,19 @@ export class HeroService {
     );
   }
 
-  // update the hero on the server
+  // PUT - update the hero on the server
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesURL, hero, this.httpOptions).pipe(
       tap(() => this.log(`updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  // POST - add a new hero to the server
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesURL, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
     );
   }
 }
